9a351b207662f2d4527acceee6cd660ef623a365
Alexander Nasonov <alnsn@yandex.ru>
2019-08-30 21:10:31 +0100

    Remove #define PROGRESS, add #define BENCHMARK.

--- t_u32toa.c
+++ t_u32toa.c
@@ -1,4 +1,4 @@
-//#define PROGRESS
+//#define BENCHMARK
 
 #include "u32toa.h"
 
@@ -8,13 +8,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifdef PROGRESS
-#include <stdio.h>
-#endif
-
+#ifndef BENCHMARK
 #define REQUIRE(x) if (!(x)) { \
 	errx(EXIT_FAILURE, "%s:%u (in %s): %s\nAborted",  \
 	    __FILE__, __LINE__, __func__, #x); }
+#endif
 
 /*
  * Each call increments a number stored in digits by 1:
@@ -56,33 +54,33 @@ int main()
 	size_t i;
 	size_t len = 1;
 	char digits[11];
-	char expected[11];
 	const char *e;
+#ifndef BENCHMARK
+	char expected[11];
+#endif
 
 	strncpy(digits, "", sizeof(digits));
-	strncpy(expected, "0", sizeof(expected));
 
 	e = u32toa(digits, 0);
+
+#ifndef BENCHMARK
+	strncpy(expected, "0", sizeof(expected));
+
 	REQUIRE(e == &digits[1]);
 	REQUIRE(memcmp(digits, expected, sizeof(expected)) == 0);
+#endif
 
 	for (i = 0; i != UINT32_MAX; i++) {
 		uint32_t n = i + 1;
-#ifdef PROGRESS
-		size_t old_len = len;
-#endif
-
-		increment(expected, &len);
-
-#ifdef PROGRESS
-		if (old_len != len)
-			printf("Testing %s (%zu digits)\n", expected, len);
-#endif
 
 		e = u32toa(digits, n);
 
+#ifndef BENCHMARK
+		increment(expected, &len);
+
 		REQUIRE(e == &digits[len]);
 		REQUIRE(memcmp(digits, expected, sizeof(expected)) == 0);
+#endif
 	}
 
 	return EXIT_SUCCESS;
